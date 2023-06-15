f2d15b95699ca7069f3328ce6204a9376ee3802c
mukesh <mukeshmehta2041@gmail.com>
2023-06-15 12:57:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { Component } from "react";
-import Movies from "./components/movies";
-import "./App.css";
+import React from "react";
+import Main from "./components/main";
 
-class App extends Component {
-  render() {
-    return (
-      <main className="container">
-        <Movies />
-      </main>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h2>Pomodoro! </h2>
+      <div className="timer"></div>
+      <div className="btn"></div>
+    </div>
+  );
+};
+
+export default Main;
